e0c3bdf35afaeee5a3ea4963ff13c17f0b55f432
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-11 21:48:36 +0100

    0. Basic annotations - add

---
+++ 0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Typed annotationsadd"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns a float"""
+    return a + b
---
+++ README.md
@@ -0,0 +1,1 @@
+https://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@github.com/ODSEFIANI/alx-backend-python.git
