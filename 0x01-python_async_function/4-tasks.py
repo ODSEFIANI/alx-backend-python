4570bdadc48dc7b7c46ab17a1535cf3741b4dbc8
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-15 23:55:32 +0100

    task vs direct countroutine

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''pymodule.
+'''
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''returns a list of waited time(float)'''
+    fwait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(fwait_times)
