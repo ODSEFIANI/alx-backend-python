4ebfc7c1fd0ac17190409430ca49180b3e073f3b
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-15 21:38:45 +0100

    0. The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 from typing import Union
 
 
-async def wait_random(max_delay: Union[float, int] = 10):
+async def wait_random(max_delay: int = 10) -> float:
 
     """waits befores ruturning random valuue"""
     await asyncio.sleep(max_delay)
