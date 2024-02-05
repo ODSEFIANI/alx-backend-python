8bca9c9e8383e06f42f297ee97e3e39e77bab6f2
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-06 00:22:32 +0100

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -61,3 +61,25 @@ class TestGetJson(unittest.TestCase):
 
             # Assert that the result of get_json is equal to the test_payload
             self.assertEqual(result, test_payload)
+class TestMemoize(unittest.TestCase):
+    """Memoize clss"""
+    def test_memoize(self) -> None:
+        """Testfunction"""
+        class TestClass:
+            def a_method(self):
+                """def
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as m_fct:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            m_fct.assert_called_once()
